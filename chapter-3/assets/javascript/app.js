585cc65f049792f5382c75317ac06f756b8fb2ba
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-17 22:00:36 +0500

    chapter 3 complete

---
+++ chapter-3/assets/javascript/app.js
@@ -0,0 +1,51 @@
+//question-1
+/*Declare a variable called age & assign to it your age. Show 
+your age in an alert box.  */
+
+/*var age = 21;
+alert("I am " + age + " years old");
+
+//second way to write this code using template literals
+
+var age = 21;
+alert(`I am ${age} years old`)*/
+
+//question-2
+/*Declare & initialize a variable to keep track of how many 
+times a visitor has visited a web page. Show his/her 
+number of visits on your web page. For example: “You 
+have visited this site N times”. */
+
+/*var number = 8;
+alert("You have visited this site " + number + " times"
+)*/
+
+
+//question-3
+/*Declare a variable called birthYear & assign to it your 
+birth year. Show the following message in your browser: */
+
+/*var birthYear = 2000;
+document.write("My birth year is " + birthYear  );
+document.write("<br>");
+document.write("Data type of my declared variable is number..");*/
+
+
+//question-4
+/* A visitor visits an online clothing store 
+www.xyzClothing.com . Write a script to store in variables 
+the following information: 
+b. Product title 
+c. Quantity i.e. how many products a visitor wants to 
+order 
+Show the following message in your browser: “John 
+Doe ordered 5 T-shirt(s) on XYZ Clothing store”. */
+
+/*var visitorName="John Doe";
+var productTitle = "T-shirt";
+var quantity = 10;
+
+document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on xyz clothing store ")*/
+
+
+
