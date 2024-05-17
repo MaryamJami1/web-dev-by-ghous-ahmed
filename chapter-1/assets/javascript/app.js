4fb946e679ca958a6a8862e478873ea8bd7397e4
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-17 16:30:09 +0500

    chapter-1 complete

---
+++ chapter-1/assets/javascript/app.js
@@ -0,0 +1,25 @@
+// question-1
+// alert("Hello, Have a good day!");
+
+// question-2
+// alert("Error! Please Enter a Valid Password");
+
+// question-3
+// alert(" Welcome to JS Land...\n Happy Coding!");
+
+// question-4
+/*alert("Welcome to JS Land.");
+alert("Happy Coding! \nprevent this page from creating additional dialouges");*/
+
+// question-5
+// alert("Hello");
+// console.log("Hello i can run js through my web browser's console");
+
+//question-6
+/*Make use of alert in your new/existing HTML & CSS 
+project. */
+// alert("Hello,Good Day! Please click ok to see the page");
+
+// question-7
+/*Practice placement of <script></script> element in 
+following sections of your project */
