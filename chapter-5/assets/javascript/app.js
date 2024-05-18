47d1aab9e4ac632dff8f22e35d1e496bdf8718b0
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-18 16:49:33 +0500

    add chapter 15

---
+++ chapter-5/assets/javascript/app.js
@@ -0,0 +1,141 @@
+//question-1
+/*Write a program that take two numbers & add them in a 
+new variable. Show the result in your browser. */
+
+/*var num1 = +prompt("Enter your first number");
+var num2 = +prompt("Enter your second number");
+var result = num1 + num2*/
+
+/*string concatination
+document.write("sum of " + num1 + " and " + num2 + " = " + result)*/
+
+/*template literals
+ document.write(`Sum of ${num1} and ${num2} = ${result}`);*/
+
+ 
+
+ ///question-2
+ /* Repeat task1 for subtraction, multiplication, division & 
+modulus. */
+
+//multiplication
+/*var num1 = prompt("Enter your first number");
+var num2 = prompt("Enter your second number");
+var result = num1 * num2;
+document.write("Multiplication of " + num1 + " * " + num2 + " = " + result );*/
+
+
+//subtraction
+/*var num1 = prompt("Enter your first number");
+var num2 = prompt("Enter your second number");
+var result = num1 - num2;
+document.write("Subtraction of " + num1 + " - " + num2 + " = " + result);*/
+
+
+//division 
+/*var num1 = prompt("Enter your first number");
+var num2 = prompt("Enter your second number");
+var result = num1 / num2;
+document.write("Division of " + num1 + " / " + num2 + " = " + result);*/
+
+
+//modulus
+/*var num1 = prompt("Enter your first number");
+var num2 = prompt("Enter your second number");
+var result = num1 % num2
+document.write("Modulus of " + num1 + " % " + num2 + " = " + result);*/
+
+
+//question-3
+/*Do the following using JS Mathematic Expressions 
+a. Declare a variable. 
+b. Show the value of variable in your browser like “Value 
+after variable declaration is: ??”. 
+c. Initialize the variable with some number. 
+d. Show the value of variable in your browser like “Initial 
+value: 5”. 
+e. Increment the variable. 
+f. Show the value of variable in your browser like “Value 
+after increment is: 6”. 
+g. Add 7 to the variable. 
+h. Show the value of variable in your browser like “Value after addition is: 13”. 
+i. Decrement the variable. 
+j. Show the value of variable in your browser like “Value 
+after decrement is: 12”. 
+k. Show the remainder after dividing the variable’s value 
+by 3.  
+l. Output : “The remainder is : 0”. */
+
+
+/*var a;
+document.write(" Value after variable declaration is: " + a);
+
+a = 5;
+document.write("<br> Initial value: " + a);
+
+++a;
+document.write("<br> Value after increment is: " + a);
+
+a = a + 7;
+document.write("<br> Value after addition is: " + a);
+
+--a;
+document.write("<br> Value after decrement is: " + a);
+
+a = a % 3;
+document.write("<br> The remainder is : " + a);*/
+
+
+
+//question-4
+/*Cost of one movie ticket is 600 PKR. Write a script to 
+store 
+ticket price in a variable & calculate the cost of buying 5 
+tickets 
+to a movie. Example output: */
+
+/*var ticketPrice = 600;
+var totalPrice = ticketPrice * 5;
+
+document.write("Total cost of buying 5 tickets to a movie is "+ totalPrice + " PKR ");*/
+
+
+
+//question-5
+/*Write a script to display multiplication table of any 
+number in your browser.  */
+
+/*document.write("<h1> <q> Table of 4 </q> </h1> ");
+document.write(" <h2> 4 x 1 = " + 4 * 1 + "<br> " + " 4 x 2 = " + 4 * 2+ "<br> " + " 4 x 3 = " + 4 * 3+ "<br> " + " 4 x 4 = " + 4 * 4+ "<br> " + " 4 x 5 = " + 4 * 5+ "<br> " + " 4 x 6 = " + 4 * 6+ "<br> " + " 4 x 7 = " + 4 * 7+ "<br> " + " 4 x 8 = " + 4 * 8+ "<br> "  + " 4 x 9 = " + 4 * 9+ "<br> " + " 4 x 10 = " + 4 * 10+ " </h2>")*/
+
+
+
+//question-6
+/*The Temperature Converter: It’s hot out! Let’s make a 
+converter based on the steps here. 
+a.  Store a Celsius temperature into a variable. 
+b. Convert it to Fahrenheit & output “NNoC is NNoF”. 
+c. Now store a Fahrenheit temperature into a variable. 
+d. Convert it to Celsius & output “NNoF is NNoC”. */
+
+
+
+
+
+
+
+
+
+
+
+//question-7
+/*Write a program to implement checkout process of a 
+shopping cart system for an e-commerce website. Store 
+the following in variables 
+
+a. Price of item 1 
+b. Price of item 2 
+c. Ordered quantity of item 1 
+d. Ordered Quantity of item 2 
+e. Shipping charges 
+Compute the total cost & show the receipt in your browser.*/
