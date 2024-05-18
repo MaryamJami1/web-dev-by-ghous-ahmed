410c164db539721d682a9eea3eddfbfc12e4b5d9
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-18 22:15:34 +0500

    add cahapter 6 to 9

---
+++ chapter-6 to 9/assests/javascript/app.js
@@ -0,0 +1,20 @@
+//question-1
+
+/*Write a program to take a number in a variable, do the 
+required arithmetic to display the following result in your 
+browser:  */
+
+var a = 10;
+document.write("<h2>RESULT</h2>")
+document.write("The value of a is " + a);
+document.write("<br>...................................................... <br>")
+document.write("<br> The value of ++a is: " +  ++a + "<br>Now the value of a is: " + a );
+document.write("<br><br><br> The value of a++ is: " + a++ + "<br>Now the value of a is: " + a);
+document.write("<br><br><br> The value of --a is: " +  --a + "<br>Now the value of a is: " + a );
+document.write("<br><br><br> The value of a-- is: " +  a-- + "<br>Now the value of a is: " + a );
+
+
+
+
+
+
