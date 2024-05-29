8be2bf390ca59efff40dd7ee13a37cc1b57835e7
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-29 23:09:29 +0500

    chapter 17 to 20

---
+++ chapter 17 to 20/javascript/app.js
@@ -0,0 +1,86 @@
+//question-1
+/*Declare and initialize an empty multidimensional array. 
+(Array of arrays) */
+
+// var emptyArr = [[], []];
+
+
+
+
+
+
+
+//question-2
+/*. Declare and initialize a multidimensional array 
+representing the following matrix:*/
+
+
+/*var outerArray = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1]];
+document.write(outerArray[0] ,"<br>" , outerArray[1],"<br>", outerArray[2]);*/
+
+
+
+
+
+
+
+
+
+//question-3
+/*Write a program to print numeric counting from 1 to 10. */
+
+/*for (let i=1; i<= 10; i++) {
+console.log(i);
+};*/
+
+
+
+
+
+
+
+
+
+
+
+//question-4
+/*Write a program to print multiplication table of any 
+number using for loop. Table number & length should be 
+taken as an input from user.   */
+
+/*var table = +prompt("Enter a number to shpw its multiplication table");
+var tableLength = +prompt("Enter length multiplication table");
+
+console.log("Multiplication table of " + table);
+console.log("Length " + tableLength);
+
+for(i = 1; i <= tableLength; i++){
+console.log(table + " x " + i + " = " + table * i);
+};*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//question-5
+/*Write a program to print items of the following array 
+using for loop: 
+fruits = [“apple”, “banana”, “mango”, “orange”, 
+“strawberry”]*/
+
+var fruitsArr = ["Apple", "Banana", "Mango", "Orange", "Strawberry"];
+
+for(i=0; i<fruitsArr.length; i++){
+console.log(fruitsArr[i])
+};
+
+console.log(lem);
