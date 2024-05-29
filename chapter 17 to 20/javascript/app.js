44547ad631aa71f073997ba63240682cd5b08139
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-30 01:56:11 +0500

    chapter 17 to 20

--- chapter 17 to 20/javascript/app.js
+++ chapter 17 to 20/javascript/app.js
@@ -77,10 +77,49 @@ using for loop:
 fruits = [“apple”, “banana”, “mango”, “orange”, 
 “strawberry”]*/
 
-var fruitsArr = ["Apple", "Banana", "Mango", "Orange", "Strawberry"];
+// var fruitsArr = ["Apple", "Banana", "Mango", "Orange", "Strawberry"];
 
-for(i=0; i<fruitsArr.length; i++){
-console.log(fruitsArr[i])
-};
+// for(i=0; i<fruitsArr.length; i++){
+// console.log(fruitsArr[i])
+// };
 
-console.log(lem);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//question-6
+/**
+ * Generate the following series in your browser. See 
+example output. 
+a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 
+b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 
+c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 
+d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 
+e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
+ */
+
+
+document.write("<h2>Counting:</h2>")
+var counting= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+for(i=0; i<=counting.length; i++){
+     document.write(counting.join(", ")[i]);
+
+}
