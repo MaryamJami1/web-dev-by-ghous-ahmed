7163989127bee58202d35e73d9a32a5e19530c08
MaryamJami1 <maryamchouhan373@gmail.com>
2024-05-30 21:21:41 +0500

    chapter 17 to 20

--- chapter 17 to 20/javascript/app.js
+++ chapter 17 to 20/javascript/app.js
@@ -118,34 +118,34 @@ e. Series: 2k, 4k, 6k, 8k, 10k, 12k, 14k, 16k, 18k, 20k
 
 
 //counting
-document.write("<h2>Counting:</h2>");
+/*document.write("<h2>Counting:</h2>");
 var counting= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 for(i=0; i<counting.length; i++){
      document.write(counting[i] + ", ");
-};
+};*/
 
 
 
 
 
 //reverse counting
-document.write("<h2>Reverse Counting:</h2>");
+/*document.write("<h2>Reverse Counting:</h2>");
 for(i=10; i >= 1; i--){
 document.write([i] + ", ")
-};
+};*/
 
 
 
 
 
 //even numbers
-document.write("<h2>Even:</h2>");
+/*document.write("<h2>Even:</h2>");
 var even= [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
 
 for(i=0; i<even.length; i++){
 document.write(even[i] + ", ");
-}
+}*/
 
 
 
@@ -160,20 +160,55 @@ document.write(i + ", ")
 };*/
 
 //using array
-document.write("<h2>Odd:</h2>");
+/*document.write("<h2>Odd:</h2>");
 var odd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
 
 for(i=0; i<odd.length; i++){
 document.write(odd[i] + ", ")
-};
+};*/
 
 
 
 
 
 //Series
-document.write("<h2>Series:</h2>");
+/*document.write("<h2>Series:</h2>");
 var series =[2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
 for(i=0; i<series.length; i++){
 document.write(series[i] + "k, ")
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//question-7
+/*You have an array 
+A = [“cake”, “apple pie”, “cookie”, “chips”, “patties”] 
+Write a program to enable “search by user input” in an 
+array. 
+After searching, prompt the user whether the given item is 
+found in the list or not. Example:
+ */
+
+var bakery = ["cake", "apple pie", "“cookie”", "“chips”", "“patties”"];
+
+var customerInput = prompt("Welcome to our bakery! What do want to order sir/ma,am ");
+
+if(bakery.includes(customerInput)){
+console.log(`your cookie is Available at index in our bakery` );
+}else{
+alert("We are sorry pastry is not available in our bakery");
 }
