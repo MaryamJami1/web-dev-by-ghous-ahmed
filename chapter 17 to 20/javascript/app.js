0e055dca0e94c65414b514e89ee56ec95f690c5b
MaryamJami1 <maryamchouhan373@gmail.com>
2024-06-01 19:52:09 +0500

    chapter 17 to 20

--- chapter 17 to 20/javascript/app.js
+++ chapter 17 to 20/javascript/app.js
@@ -203,7 +203,7 @@ After searching, prompt the user whether the given item is
 found in the list or not. Example:
  */
 
-var bakery = ["cake", "apple pie", "cookie", "chips", "patties"];
+/*var bakery = ["cake", "apple pie", "cookie", "chips", "patties"];
 var customerInput = prompt("Welcome to our bakery! What do you want to order sir/ma'am?")
 var index = bakery.indexOf(customerInput);
 
@@ -211,7 +211,7 @@ if(bakery.includes(customerInput)){
 alert(`${customerInput} is available at index ${index} in our bakery`)
 }else{
 alert(`${customerInput} is not available in our bakery`)
-};
+};*/
 
 
 
