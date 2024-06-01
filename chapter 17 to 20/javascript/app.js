e2d0d9f9b8fa8f15e283995dcb51be594df0eacb
MaryamJami1 <maryamchouhan373@gmail.com>
2024-06-01 20:32:30 +0500

    chapter 17 to 20

--- chapter 17 to 20/javascript/app.js
+++ chapter 17 to 20/javascript/app.js
@@ -223,10 +223,9 @@ alert(`${customerInput} is not available in our bakery`)
 given array. 
 A = [24, 53, 78, 91, 12]. */
 
-var A = [24, 53, 78, 91, 12];
+/*var A = [24, 53, 78, 91, 12];
 document.write("Array items: "+ A);
-document.write("<br> The largest number is: "+ Math.max(...A));
-
+document.write("<br> The largest number is: "+ Math.max(...A));*/
 
 
 
