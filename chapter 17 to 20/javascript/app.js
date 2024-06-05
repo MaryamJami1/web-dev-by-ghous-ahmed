7944bcdcc07553731d69e92df36cd2a1e58d9ee8
MaryamJami1 <maryamchouhan373@gmail.com>
2024-06-05 21:29:37 +0500

    chapter 17 to 20

--- chapter 17 to 20/javascript/app.js
+++ chapter 17 to 20/javascript/app.js
@@ -362,8 +362,25 @@ for(i=0; i < printStr.length; i++){
 // *****
 
 
-for(var i=1; i<=5; i++){
-for(var j=1; j<=i; j++){
+// for(var i=1; i<=5; i++){
+// for(var j=1; j<=i; j++){
+// document.write("*")
+// }
+// document.write("<br>")
+// }
+
+
+
+
+// *****
+// ****
+// ***
+// **
+// *
+
+
+for(var i=1; i<=10; i++){
+for(var j=10; j>=i; j-- ){
 document.write("*")
 }
 document.write("<br>")
