4492a4f0b7aef4f867f261d98b800b6c4101927a
MaryamJami1 <maryamchouhan373@gmail.com>
2024-07-04 21:25:54 +0500

    ch 31 to 34

--- chapter-31-to-34/app.js
+++ chapter-31-to-34/app.js
@@ -23,3 +23,21 @@
 // var arrDay = ["Sun","Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"]
 // var day = date.getDay()
 // console.log("Today is: " + arrDay[day])
+
+
+
+
+
+
+
+//question-4
+// var date = new Date();
+// var arrDay = ["Sun","Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"]
+// var day = date.getDay()
+// if(day == 0 || day == 6){
+//      console.log("Today is fun day")
+// }else{
+//      console.log("Today is working day")
+// }
+
+
