6bc229465ad69b2f837259ed344cf71293a1b20c
MaryamJami1 <maryamchouhan373@gmail.com>
2024-07-04 20:13:12 +0500

    chapter 21 to 30

---
+++ chapter 21-to-30/app.js
@@ -0,0 +1,11 @@
+////////////string method
+
+// question-1
+var firstName = prompt("Enter your first name");
+var lastName = prompt("Enter your last name"); 
+var fullName = `${firstName} ${lastName}`;
+var greet = alert(`Hello ${fullName} How was your day?`);
+
+
+
+// question-2
