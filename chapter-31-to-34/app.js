75380cfd09781291c82d3a74a73c0c9b8d13c9cd
MaryamJami1 <maryamchouhan373@gmail.com>
2024-07-04 21:22:38 +0500

    ch 31 to 34

--- chapter-31-to-34/app.js
+++ chapter-31-to-34/app.js
@@ -13,3 +13,13 @@
 // var arrMon = ["January","February ", "March", "April", "May", "Junr", "July", "August", "September", "October", "November", "December"]
 // var mon = date.getMonth()
 // console.log(arrMon[mon])
+
+
+
+
+
+//question-3
+// var date = new Date();
+// var arrDay = ["Sun","Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"]
+// var day = date.getDay()
+// console.log("Today is: " + arrDay[day])
