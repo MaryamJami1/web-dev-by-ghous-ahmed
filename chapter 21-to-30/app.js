cfb8659bcc46203ff16400d4bed1ac7eeb25ecaa
MaryamJami1 <maryamchouhan373@gmail.com>
2024-07-04 20:18:45 +0500

    chapter 21 to 30

--- chapter 21-to-30/app.js
+++ chapter 21-to-30/app.js
@@ -1,11 +1,27 @@
 ////////////string method
 
 // question-1
-var firstName = prompt("Enter your first name");
-var lastName = prompt("Enter your last name"); 
-var fullName = `${firstName} ${lastName}`;
-var greet = alert(`Hello ${fullName} How was your day?`);
+// var firstName = prompt("Enter your first name");
+// var lastName = prompt("Enter your last name"); 
+// var fullName = `${firstName} ${lastName}`;
+// var greet = alert(`Hello ${fullName} How was your day?`);
 
 
 
 // question-2
+// var mobilePhone = prompt("Enter your favourite mobile phone");
+
+// var favouritePhone = `My favourite phone is: ${mobilePhone}`
+
+// console.log(favouritePhone)
+
+// var mobileLength = favouritePhone.length;
+// console.log(`Full length is: ${mobileLength}`)
+
+// var inputLength = mobilePhone.length 
+// console.log(`String length is: ${inputLength}`)
+
+
+
+
+//question-3
