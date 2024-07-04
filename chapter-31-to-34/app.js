8ef5e793c2f0ba68236b839781b55a42b7b60975
MaryamJami1 <maryamchouhan373@gmail.com>
2024-07-04 21:18:09 +0500

    ch 31 to 34

--- chapter-31-to-34/app.js
+++ chapter-31-to-34/app.js
@@ -4,3 +4,12 @@
 //question-1
 // var date = new Date();
 // console.log(date)
+
+
+
+
+//question-2
+// var date = new Date();
+// var arrMon = ["January","February ", "March", "April", "May", "Junr", "July", "August", "September", "October", "November", "December"]
+// var mon = date.getMonth()
+// console.log(arrMon[mon])
