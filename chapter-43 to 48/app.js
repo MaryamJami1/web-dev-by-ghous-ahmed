65e6bd7df6a5dc8a14e0ac9a12fa3f1ca797b6e8
MaryamJami1 <maryamchouhan373@gmail.com>
2024-08-03 20:36:24 +0500

    chapter 43 to 48

--- chapter-43 to 48/app.js
+++ chapter-43 to 48/app.js
@@ -1,12 +1,44 @@
 // question one
 
-function greet(){
+function greet() {
     alert("Hello! How are you?")
 }
 
 
+
+
+
+
+
+
+
+
+
 // question-3
-function deleteRow(button) {
-    var row = button.parentNode.parentNode;
-    row.parentNode.removeChild(row);
-  }
+
+function deleteRow(row) {
+    row.parentNode.parentNode.remove()
+}
+
+
+
+
+
+
+// question-5
+
+let count = 0;
+
+function increase() {
+    document.getElementById("counter").innerText = ++count
+}
+
+
+
+function decrease(){
+    if(count>0){
+        document.getElementById("counter").innerText= --count
+    }else{
+        alert("cart is empty")
+    }
+}
