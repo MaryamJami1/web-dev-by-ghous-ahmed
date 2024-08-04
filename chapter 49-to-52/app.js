98cba6558e0bb37d2b2d0e1fd2fb7cefef03e96a
MaryamJami1 <maryamchouhan373@gmail.com>
2024-08-04 20:43:35 +0500

    chapter 49 to 59

---
+++ chapter 49-to-52/app.js
@@ -0,0 +1,18 @@
+// question-one
+
+function getValues() {
+    let name = document.getElementById("name").value;
+    let lastName = document.getElementById("last-name").value;
+    let email = document.getElementById("Email").value;
+    let password = document.getElementById("password").value
+    alert(`First Name: ${name}``Last Name: ${lastName}``Email: ${email}``Password: ${password}`)
+}
+
+
+
+
+// question-2
+function readMore() {
+    let story = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem quasi porro in veritatis minus,Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem quasi porro in veritatis minus,Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem quasi porro in veritatis minus,Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem quasi porro in veritatis minus,Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem quasi porro in veritatis minus,Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem quasi porro in veritatis minus,Lorem ipsum, dolor sit amet consectetur adipisicing elit.  elit. Autem quasi porro in veritatis minus, voluptatem eius ea nostrum unde eveniet deserunt reprehenderit tenetur vero doloribus eaque architecto aspernatur odio adipisci dicta natus quas voluptatibus at asperiores? Ipsum soluta autem ad quo unde. Eaque incidunt dolorum sed doloremque velit ex molestiae magnam similique laudantium temporibus facilis maxime blanditiis officiis sint perspiciatis, quasi sit doloribus cupiditate ratione animi expedita ab. Sapiente similique omnis natus assumenda mollitia. Similique accusantium dicta culpa fugiat, nam iste? Totam voluptatibus ipsa veritatis, distinctio doloremque aspernatur aut enim eos labore perferendis natus inventore iusto ad molestias voluptate ut nobis aliquam eius. Reiciendis harum vitae error minus aliquid, beatae praesentium odio quidem velit maxime molestias ad, facilis ullam illum est doloremque a ducimus, quisquam nihil debitis veniam? Laboriosam expedita alias vel necessitatibus, veniam consectetur quasi labore deleniti eligendi laborum quo, vero quibusdam nostrum dolore? Cumque quo voluptate dolor illo nemo, mollitia, accusantium reiciendis consectetur ea ab sapiente aspernatur, animi at maiores pariatur doloremque laborum neque? Accusamus dolores sequi exercitationem ipsum, repellat possimus cumque mollitia veritatis corrupti velit nulla laborum recusandae autem nihil, quos iure, unde corporis provident consequuntur iste aperiam excepturi sapiente rem. Adipisci ex maxime quas Autem quasi porro in veritatis minus,"
+    document.getElementById("story").innerText = story
+}
